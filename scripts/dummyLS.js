68a2b6a0722f27b2ade133158c885278ea6c8ac6
Denny Cave <denny@caveim.com>
2022-05-09 22:11:42 -0500

    Update dummyLS.js

--- scripts/dummyLS.js
+++ scripts/dummyLS.js
@@ -38,188 +38,81 @@ for (var i = 0, n = quota.length; i < n; i++) {
 var url = baseUrl + "/configs/settings.yaml";
 var settings = toNative(new Yaml().load(new Transport().get(url)));
 var fields = settings.fields;
-if (group.groupType == 'trial') {
-    
-    fields.push({
-      "type": "displayfield",
-      "cls": "warning",
-      "height": 30,
-      "hideLabel": true,
-      "markup": "Not available for " + group.groupType + " account. Please upgrade your account."
-    })
- 
-    if (isLS.result == 0 || isLS.result == Response.PERMISSION_DENIED) {
-        settings.fields.push({
-            "type": "compositefield",
-            "hideLabel": true,
-            "pack": "left",
-            "itemCls": "deploy-manager-grid",
-            "cls": "x-grid3-row-unselected",
-            "items": [{
-                "type": "spacer",
-                "width": 4
-            }, {
-                "type": "displayfield",
-                "cls": "x-grid3-row-checker x-item-disabled",
-                "width": 30,
-                "height": 20
-            }, {
-                "type": "displayfield",
-                "cls": "x-item-disabled",
-                "value": lsText
-            }]
-        });
-    }
 
-    settings.fields.push({
-        "type": "compositefield",
-        "hideLabel": true,
-        "pack": "left",
-        "itemCls": "deploy-manager-grid",
-        "cls": "x-grid3-row-unselected",
-        "items": [{
-            "type": "spacer",
-            "width": 4
-        }, {
-            "type": "displayfield",
-            "cls": "x-grid3-row-checker x-item-disabled",
-            "width": 30,
-            "height": 20
-        }, {
-            "type": "displayfield",
-            "cls": "x-item-disabled",
-            "value": dbText
-        }]
-    });
- 
-    if (isCDN.result == 0 || isCDN.result == Response.PERMISSION_DENIED) {
-        settings.fields.push({
-            "type": "compositefield",
-            "hideLabel": true,
-            "pack": "left",
-            "itemCls": "deploy-manager-grid",
-            "cls": "x-grid3-row-unselected",
-            "items": [{
-                "type": "spacer",
-                "width": 4
-            }, {
-                "type": "displayfield",
-                "cls": "x-grid3-row-checker x-item-disabled",
-                "width": 30,
-                "height": 20
-            }, {
-                "type": "displayfield",
-                "cls": "x-item-disabled",
-                "value": cdnText
-            }]
-        });
-    }
-    
-    settings.fields.push({
-        "type": "compositefield",
-        "hideLabel": true,
-        "pack": "left",
-        "itemCls": "deploy-manager-grid",
-        "cls": "x-grid3-row-unselected",
-        "items": [{
-            "type": "spacer",
-            "width": 4
-        }, {
-            "type": "displayfield",
-            "cls": "x-grid3-row-checker x-item-disabled",
-            "width": 30,
-            "height": 20
-        }, {
-            "type": "displayfield",
-            "cls": "x-item-disabled",
-            "value": sslText
-        }]
-    });
-
+if (isLS.result == 0 || isLS.result == Response.PERMISSION_DENIED) {
     settings.fields.push({
         type: "checkbox",
-        name: "mu-addon",
-        caption: muText,
-        value: false
-
-    });
-
-} else {
-
-    if (isLS.result == 0 || isLS.result == Response.PERMISSION_DENIED) {
-        settings.fields.push({
-            type: "checkbox",
-            name: "ls-addon",
-            caption: lsText,
-            value: true,
-            tooltip: "If this option is disabled, the cluster will be installed using NGINX load balancer and application servers",
-            "showIf": {
-                "true": {
-                    "type": "checkbox",
-                    "name": "waf",
-                    "caption": wafText,
-                    "value": true,
-                    "tooltip": "Protect web sites with <a href='https://www.litespeedtech.com/support/wiki/doku.php/litespeed_wiki:waf'>LiteSpeed built-in WAF</a> based on Free ModSecurity Rules from Comodo"
-                },
-                "false": {
-                    "type": "compositefield",
-                    "hideLabel": true,
-                    "pack": "left",
-                    "name": "waf",
-                    "value": false,
-                    "itemCls": "deploy-manager-grid",
-                    "cls": "x-grid3-row-unselected",
-                    "items": [{
-                        "type": "displayfield",
-                        "cls": "x-grid3-row-checker x-item-disabled",
-                        "margins": "0 0 0 -3",
-                        "width": 16,
-                        "height": 20
-                        
-                    }, {
-                        "type": "displayfield",
-                        "cls": "x-item-disabled",
-                        "value": wafText,
-                        "margins": "0 0 0 12"
-                    }]
-                }   
-             }
-        });
-    }
-
-    settings.fields.push({
-        type: "checkbox",
-        name: "galera",
-        caption: dbText,
+        name: "ls-addon",
+        caption: lsText,
         value: true,
-        tooltip: "<h3>Requirements for All Tables:</h3> * run on InnoDB storage engine <p>* have a primary key</p>Read more about <a href='https://mariadb.com/kb/en/library/mariadb-galera-cluster-known-limitations/'>limitations</a>"
-    });
-   
-    if (isCDN.result == 0 || isCDN.result == Response.PERMISSION_DENIED) {
-        settings.fields.push({
-            type: "checkbox",
-            name: "cdn-addon",
-            caption: cdnText,
-            value: true
-        });
-    }
-
-    var resp = jelastic.billing.account.GetQuotas('environment.externalip.enabled');
-    if (resp.result == 0 && resp.array[0].value) {
-        fields.push({
-            type: "checkbox",
-            name: "le-addon",
-            caption: sslText,
-            value: true
-        });
-    }
-    settings.fields.push({
-        type: "checkbox",
-        name: "mu-addon",
-        caption: muText,
-        value: false
+        tooltip: "If this option is disabled, the cluster will be installed using NGINX load balancer and application servers",
+        "showIf": {
+            "true": {
+                "type": "checkbox",
+                "name": "waf",
+                "caption": wafText,
+                "value": true,
+                "tooltip": "Protect web sites with <a href='https://www.litespeedtech.com/support/wiki/doku.php/litespeed_wiki:waf'>LiteSpeed built-in WAF</a> based on Free ModSecurity Rules from Comodo"
+            },
+            "false": {
+                "type": "compositefield",
+                "hideLabel": true,
+                "pack": "left",
+                "name": "waf",
+                "value": false,
+                "itemCls": "deploy-manager-grid",
+                "cls": "x-grid3-row-unselected",
+                "items": [{
+                    "type": "displayfield",
+                    "cls": "x-grid3-row-checker x-item-disabled",
+                    "margins": "0 0 0 -3",
+                    "width": 16,
+                    "height": 20
+                    
+                }, {
+                    "type": "displayfield",
+                    "cls": "x-item-disabled",
+                    "value": wafText,
+                    "margins": "0 0 0 12"
+                }]
+            }   
+         }
     });
 }
+
+settings.fields.push({
+    type: "checkbox",
+    name: "galera",
+    caption: dbText,
+    value: true,
+    tooltip: "<h3>Requirements for All Tables:</h3> * run on InnoDB storage engine <p>* have a primary key</p>Read more about <a href='https://mariadb.com/kb/en/library/mariadb-galera-cluster-known-limitations/'>limitations</a>"
+});
+
+if (isCDN.result == 0 || isCDN.result == Response.PERMISSION_DENIED) {
+    settings.fields.push({
+        type: "checkbox",
+        name: "cdn-addon",
+        caption: cdnText,
+        value: true
+    });
+}
+
+var resp = jelastic.billing.account.GetQuotas('environment.externalip.enabled');
+if (resp.result == 0 && resp.array[0].value) {
+    fields.push({
+        type: "checkbox",
+        name: "le-addon",
+        caption: sslText,
+        value: true
+    });
+}
+settings.fields.push({
+    type: "checkbox",
+    name: "mu-addon",
+    caption: muText,
+    value: false
+});
+
 
 if (quotaText) {
     settings.fields.push(
